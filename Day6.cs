127a114ef47830a2b0b9f1cda7f47cfc6270ba56
Frederik <frederik290@gmail.com>
2020-12-26 14:15:25 +0100

    Completed Day6

--- Day6.cs
+++ Day6.cs
@@ -8,48 +8,77 @@ public class CustomCustoms
     {
         var inputString = InputReader.GetInputStringForDay("Day6");
         var groupAnswers = GetGroupAnswers(inputString).ToList();
-        var totalNumberOfYesAnswers = 0;
+        var totalNumberOfYesAnswersGivenByEveryone = 0;
+        var totalNumberOfYesAnswersGivenByAnyone = 0;
 
         foreach (var groupAnswer in groupAnswers)
         {
-           var answersForGroup = GetNumberOfYesAnswersFromGroup(groupAnswer);
-           totalNumberOfYesAnswers += answersForGroup;
+            var numberOfAnswersGivenByEveryone = GetNumberOfYesAnswersFromGroup(groupAnswer, YesAnswers.EVERYONE);
+            totalNumberOfYesAnswersGivenByEveryone += numberOfAnswersGivenByEveryone;
+
+            var numberOfAnswersGivenByAnyone = GetNumberOfYesAnswersFromGroup(groupAnswer, YesAnswers.ANYONE);
+            totalNumberOfYesAnswersGivenByAnyone+= numberOfAnswersGivenByAnyone;
         }
 
-        $"{totalNumberOfYesAnswers} is the total number of YES answers".Dump();
+        $"{totalNumberOfYesAnswersGivenByEveryone} is the total number of YES answers given by everyone".Dump();
+        $"{totalNumberOfYesAnswersGivenByAnyone} is the total number of YES answers given by anyone".Dump();
 
     }
 
-    private int GetNumberOfYesAnswersFromGroup(string groupAnswer)
+    private int GetNumberOfYesAnswersFromGroup(string groupAnswer, YesAnswers countType)
     {
+        var personAnswers = groupAnswer.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        var numberOfYesAnswers = 0;
         
+        if(countType.Equals(YesAnswers.ANYONE))
+            numberOfYesAnswers = GetAnswersByUntion(personAnswers);
+
+        if(countType.Equals(YesAnswers.EVERYONE))
+            numberOfYesAnswers = GetAnswersByIntersection(personAnswers);
+
+        return numberOfYesAnswers;
+    }
+
+    private int GetAnswersByIntersection(string[] personAnswers)
+    {
+        var charLists = new List<List<char>>();
+        var result = new List<char>();
+
+        foreach (var answerString in personAnswers)
+            charLists.Add(answerString.ToCharArray().ToList());
+        
+        result = charLists.First();
+
+        for (int i = 1; i < charLists.Count; i++)
+        {
+            var current = charLists[i];
+            result = result.Intersect(current).ToList();
+        }
+        return result.Count;
+    }
+
+    private int GetAnswersByUntion(string[] personAnswers)
+    {
         var setOfGroupAnswers = new HashSet<char>();
-        var personAnswers = groupAnswer.Split(new string[]{Environment.NewLine}, StringSplitOptions.None);
-        
         foreach (var personAnswer in personAnswers)
         {
             var markedQuestions = personAnswer.ToCharArray();
             setOfGroupAnswers.UnionWith(markedQuestions);
         }
-        
-        //To help get a sense of what is going on comment out the line below
-        //DumpGroupInfo(groupAnswer, setOfGroupAnswers);
-
         return setOfGroupAnswers.Count();
+
+       
     }
 
-     private string[] GetGroupAnswers(string inputString)
+    private string[] GetGroupAnswers(string inputString)
     {
         var separator = new string[] { Environment.NewLine + Environment.NewLine };
         return inputString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
     }
+}
 
-    private void DumpGroupInfo(string groupAnswer, HashSet<char> setOfGroupAnswers)
-    {
-        "######## Group answer string ########".Dump();
-        groupAnswer.Dump();
-        "######### Distinct YES answers ##########".Dump();
-        setOfGroupAnswers.ToList().ForEach(i => i.ToString().Dump());
-        $"Number distinct YES answers in group: {setOfGroupAnswers.Count()}".Dump();
-    }
+public enum YesAnswers
+{
+    EVERYONE,
+    ANYONE
 }
